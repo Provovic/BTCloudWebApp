a7c337c6f0c5e39d5d1dcf48c4c733d7d29030eb
Brandon Torres <brandontorres.software@gmail.com>
2024-01-17 22:55:31 -0600

    FIRST

---
+++ BTCloudWebApp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BTCloudWebApp.Pages;
+
+public class WeatherTrackingModel : PageModel
+{
+    private readonly ILogger<WeatherTrackingModel> _logger;
+
+    public WeatherTrackingModel(ILogger<WeatherTrackingModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
